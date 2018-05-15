50de090f4570dd1356706e4e5fcd94b81a6e8bb0
Larson Carter <larsoncarter@blazermail.net>
2018-05-14 21:33:25 -0500

    Add files via upload

--- My Web Browser/My Web Browser/ViewController.swift
+++ My Web Browser/My Web Browser/ViewController.swift
@@ -7,85 +7,50 @@
 //
 
 import Cocoa
-
 import WebKit
-
 class ViewController: NSViewController {
-    
     @IBOutlet weak var webview: WKWebView!
-    
     @IBOutlet var desiredWebsite: NSTextField!
-    
     @IBOutlet var goBackButton: NSButton!
-    
     @IBOutlet var goHomeButton: NSButton!
-    
     @IBOutlet var goForwardButton: NSButton!
-    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
         let initialurl = "https://google.com"
-        
         let url = URL(string: String(initialurl))
-        
         let request = URLRequest(url: url!)
-        
         webview.load(request)
-        
         desiredWebsite.stringValue = initialurl
-        
     }
-    
     @IBAction func desiredWebsiteOnTouch(_ sender: Any) {
-        
         let gotoweb = desiredWebsite.stringValue
-        
         let url = URL(string: String(gotoweb))
-        
         let request = URLRequest(url: url!)
-        
         webview.load(request)
-        
         desiredWebsite.stringValue = gotoweb
-        
     }
-    
     @IBAction func goHomeOnTouch(_ sender: Any) {
-        
         let gotohome = "https://google.com"
-        
         let url = URL(string: String(gotohome))
-        
         let request = URLRequest(url: url!)
-        
         webview.load(request)
-        
         desiredWebsite.stringValue = gotohome
-        
     }
-    
     @IBAction func goBackOnTouch(_ sender: Any) {
-        
         let gotoback = ""
-        
         let url = URL(string: String(gotoback))
-        
         let request = URLRequest(url: url!)
-        
         webview.load(request)
-        
         desiredWebsite.stringValue = gotoback
-        
     }
-    
     override var representedObject: Any? {
-        
         didSet {
-            
+            /*
+            let updateDesiredWebsite = 0
+            while (updateDesiredWebsite < 1) {
+                desiredWebsite.stringValue = // What The Wbe View Currently Shows
+            }
+            */
         }
-        
     }
-    
 }
